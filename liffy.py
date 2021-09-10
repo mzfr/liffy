26fec4bb2cb5e33c9aea79f8588df341745dba4c
Jakom <48088579+RyouYoo@users.noreply.github.com>
2021-09-10 18:00:02 +0100

    Update liffy.py

--- liffy.py
+++ liffy.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python
 
-import argparse
-import os
-import signal
-import sys
-import urllib.parse
+import argparse, os, signal, sys, urllib.parse
 
 from pyfiglet import figlet_format
 
@@ -22,11 +18,7 @@ def ping(hostname):
         bool -- Tell if host is up or not
     """
     resp = os.system("ping -c 1 -W2 "+hostname+" > /dev/null 2>&1")
-
-    if resp == 0:
-        return True
-    else:
-        return False
+    return resp == 0
 
 
 def signal_handler(signal, frame):
@@ -35,11 +27,6 @@ def signal_handler(signal, frame):
 
 
 def main():
-    if not len(sys.argv):
-        print("[!] Not Enough Arguments!")
-        # TODO: Add usage
-        sys.exit(0)
-
     parser = argparse.ArgumentParser()
     parser.add_argument("url", help="URL to test for LFI")
     parser.add_argument("-d", "--data", help="Use data:// technique", action="store_true")
@@ -55,7 +42,8 @@ def main():
     parser.add_argument("--cookies", help="session cookies for authentication")
     parser.add_argument('-dt', "--directorytraverse", help="Test for Directory Traversal", action="store_true")
 
-    args = parser.parse_args()
+    # Show the help message if the user does not provide any arguments.
+    args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 
     url = args.url
     nostager = args.nostager
@@ -78,20 +66,16 @@ def main():
     # TODO: Find a better way to do these checks
     if args.data:
         print(colors("[~] Testing with data:// ", 93))
-        d = data.Data(url, nostager, cookies)
-        d.execute_data()
+        data.Data(url, nostager, cookies).execute_data()
     elif args.input:
         print(colors("[~] Testing with input:// ", 93))
-        i = Input.Input(url, nostager, cookies)
-        i.execute_input()
+        Input.Input(url, nostager, cookies).execute_input()
     elif args.expect:
         print(colors("[~] Testing with expect:// ", 93))
-        e = Expect.Expect(url, nostager, cookies)
-        e.execute_expect()
+        Expect.Expect(url, nostager, cookies).execute_expect()
     elif args.proc:
         print(colors("[~] /proc/self/environ Technique Selected!", 93))
-        i = proc.Environ(url, nostager, relative, cookies)
-        i.execute_environ()
+        proc.Environ(url, nostager, relative, cookies).execute_environ()
     elif args.access:
         print(colors("[~] Testing for Apache access.log poisoning", 93))
         if not args.location:
@@ -99,8 +83,7 @@ def main():
             l = '/var/log/apache2/access.log'
         else:
             l = args.location
-        a = accesslog(url, l, nostager, relative, cookies)
-        a.execute_logs()
+        accesslog(url, l, nostager, relative, cookies).execute_logs()
     elif args.ssh:
         print(colors("[~] Testing for SSH log poisoning ", 93))
         if not args.location:
@@ -108,24 +91,18 @@ def main():
             l = '/var/log/auth.log'
         else:
             l = args.location
-        a = sshlog.SSHLogs(url, l, relative, cookies)
-        a.execute_ssh()
+        sshlog.SSHLogs(url, l, relative, cookies).execute_ssh()
     elif args.filter:
         print(colors("[~] Testing with expect://", 93))
-        f = Filter.Filter(url, cookies)
-        f.execute_filter()
+        Filter.Filter(url, cookies).execute_filter()
     elif args.directorytraverse:
         print(colors("[~] Testing for directory traversal", 93))
-        filename = input(colors("[*] Please give a payload file for testing Directory Traversl: ", 91))
-        dt = DirTraversal.dirTraversal(url, filename, True)
-        dt.execute_dirTraversal()
+        DirTraversal.dirTraversal(url, input(colors("[*] Please give a payload file for testing Directory Traversl: ", 91)), True).execute_dirTraversal()
     else:
         print(colors("[!] Please select atleast one technique to test", 91))
         sys.exit(0)
 
-
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
-    print(colors(figlet_format('Liffy v2.0', font='big'), 92))
-    print("\n")
+    print(colors(figlet_format('Liffy v2.0', font='big'), 92), "\n")
     main()
